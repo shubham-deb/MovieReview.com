af2d8eaaad3b0653440cee76b3600cd5405c3b57
SHUBHAM DEB <debshubham@gmail.com>
2017-06-28 12:09:01 -0400

    added project

--- MovieReviews/server/app.js
+++ MovieReviews/server/app.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://localhost/MovieReviewProject');
+// mongoose.connect('mongodb://localhost/MovieReviewProject');
 
-// mongoose.connect('mongodb://shabbirsaifee:Barcelona_123@ds139781.mlab.com:39781/heroku_gvpdptmh');
+mongoose.connect('mongodb://shabbirsaifee:Barcelona_123@ds139781.mlab.com:39781/heroku_gvpdptmh');
 
 mongoose.connection.on('open',function () {
 console.log('Movie Review Database Connected');
