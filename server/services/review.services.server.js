d3e543baf1c1dab4a5e909d6f82f5ef9557a9bcc
Shubham Deb <debshubham@ccs.neu.edu>
2018-09-13 11:13:48 -0400

    made changes

---
+++ server/services/review.services.server.js
@@ -0,0 +1,57 @@
+
+var app = require('../../express');
+var reviewModel = require('../../model/review/review.model.server');
+//--------------------------------------------------------------------
+    //------ALL URIs--------------------
+
+app.post('api/project/createReview',createReview);
+app.delete('api/project/deleteReview/:reviewId',deleteReview);
+app.put('api/project/editReview/:reviewId',editReview);
+app.get('api/project/getReview/:userId',getReview);
+app.get('api/project/getAllReviews/:reviewId',getAllReviews);
+//----------------------------------------------
+
+function createReview(req,res) {
+    var review = req.body;
+    reviewModel
+        .createReview(review)
+        .then(function (review) {
+            res.json(review);
+        })
+}
+
+function deleteReview(req,res) {
+
+    var reviewId = req.params['reviewId'];
+    reviewModel.deleteReview(reviewId)
+        .then(function () {
+            res.sendStatus(200);
+        })
+}
+
+function editReview(req,res) {
+    var review = req.body;
+    var reviewId = req.params['reviewId'];
+    reviewModel.editReview(reviewId,review)
+        .then(function () {
+            res.sendStatus(200);
+        })
+}
+
+function getReview(req,res) {
+    var userId = req.params['userId'];
+    reviewModel.getUserReview(userId)
+        .then(function () {
+            res.sendStatus(200);
+        });
+}
+
+function getAllReviews(req,res) {
+    var userId = req.params['userId'];
+    reviewModel.getUserReviews(userId)
+        .then(function () {
+            res.sendStatus(200);
+        });
+}
+
+
